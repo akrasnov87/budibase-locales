f976f6cbe3171b8040750486d9890453750d72b8
Александр Краснов <a-krasnov@it-serv.ru>
2024-07-16 07:08:26 +0300

    изменена инициализация локали

--- modules/svelte.js
+++ modules/svelte.js
@@ -1,11 +1,11 @@
 import { register, init, getLocaleFromNavigator } from "svelte-i18n"
 
-export function initSvelteLocales () {
-    register("en", () => import(`../locales/en.json`))
-    register("ru", () => import(`../locales/ru.json`))
+export function initSvelteLocales() {
+  register("en", () => import(`../locales/en.json`))
+  register("ru", () => import(`../locales/ru.json`))
 
-    init({
-        fallbackLocale: "en",
-        initialLocale: window.BB.lang || getLocaleFromNavigator(),
-    });
+  init({
+    //fallbackLocale: "en",
+    initialLocale: window.BB.lang || getLocaleFromNavigator(),
+  })
 }
